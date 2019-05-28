6a00d6e02776658dd916e34a338cd95f563ad942
FilipPichPython <fpich72@gmail.com>
2019-05-28 12:32:54 +0200

    epierce intro

---
+++ src/main/java/com/epierce/sizes/SizesApplication.java
@@ -0,0 +1,13 @@
+package com.epierce.sizes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SizesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SizesApplication.class, args);
+    }
+
+}
